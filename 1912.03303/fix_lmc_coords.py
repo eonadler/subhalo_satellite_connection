6d1af8b1c2483e23481f0be3bf9a69c33290c7d9
Ethan Nadler <enadler@stanford.edu>
2020-04-01 20:20:45 -0700

    general cleanup

--- 1912.03303/fix_lmc_coords.py
+++ 1912.03303/fix_lmc_coords.py
@@ -2,6 +2,59 @@ import numpy as np
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 
+def transform_to_observer_frame(pos,observer_distance=8.,observer_locations=6):
+	"""
+    Transforms Galactocentric position vectors to the frame of a randomly oriented heliocentric observer at the correct Galactocentric distance
+
+    Args:
+    pos (array): array of Galactocentric satellite positions
+    observer_distance: Galactocentric distance of sun (units of kpc)
+    observer_locations: number of observer locations to choose from
+
+    Returns:
+    pos_observer_frame (array): array of satellite position vectors in heliocentric frame
+    """
+	x = np.zeros((observer_locations,3))
+
+	for i in range(observer_locations/2):
+        x[i*2][i] = observer_distance
+        x[i*2+1][i] = -1.*observer_distance
+
+	pos_observer_frame = pos - x[np.random.randint(0,observer_locations)]
+
+	return pos_observer_frame
+
+def rotate_about_vector(pos,vec1,vec2):
+	"""
+    Returns heliocentric satellite position vectors transformed via a rotation matrix that rotates the mock LMC analog into its correct sky position
+
+    Args:
+    pos (array): array of heliocentric satellite position vectors
+    vec1 (array): original heliocentric coordinates of mock LMC
+    vec2 (array): final heliocentric coordinates of mock LMC
+
+    Returns:
+    rotated_pos (array): array of position vectors in rotated frame
+    """
+	rotated_pos = np.zeros(np.shape(pos))
+
+	a,b = (vec1/np.linalg.norm(vec1)), (vec2/np.linalg.norm(vec2))
+    
+	v = np.cross(a,b)
+	c = np.dot(a,b)
+	s = np.linalg.norm(v)
+	I = np.identity(3)
+
+	vXStr = '{} {} {}; {} {} {}; {} {} {}'.format(0., -v[2], v[1], v[2], 0., -v[0], -v[1], v[0], 0.)
+	K = np.matrix(vXStr)
+	R = I + k + np.matmul(K,K) * ((1.-c)/(s**2))
+
+	for i in range (0,len(rotated_pos)):
+        temp_vec = (pos[i][0],pos[i][1],pos[i][2])
+        (rotated_pos[i][0],rotated_pos[i][1],rotated_pos[i][2]) = np.asarray(R*np.reshape(temp_vec,(3,1)))[:,0]
+
+    return rotated_pos
+
 def get_lmc_coords(halo_data,cosmo_params,lmc_true_sky_coords='05:23:34.5 -69:45.37'):
     """
     Returns Cartesian coordinates of LMC analog assuming (a) it is fixed to the sky position of the true LMC, and (b) the true LMC is at the distance of the mock LMC
@@ -25,7 +78,7 @@ def get_lmc_coords(halo_data,cosmo_params,lmc_true_sky_coords='05:23:34.5 -69:45
 
 	return lmc_cartesian_coords
 
-def rotate_about_LMC(satellite_properties,lmc_cartesian_coords,observer_distance=8.,observer_locations=6):
+def rotate_about_LMC(satellite_properties,lmc_cartesian_coords):
     """
     Returns Cartesian coordinates of mock satellites, rotated such that the LMC is fixed at the Cartesian coordinates from the previous function
 
@@ -36,38 +89,17 @@ def rotate_about_LMC(satellite_properties,lmc_cartesian_coords,observer_distance
     Returns:
     satellite_properties (dict): dict containing properties of satellites including rotated positions
     """
-	#Load satellite positions
 	pos = satellite_properties['pos']
-	rotated_pos = np.zeros((np.shape(pos)))
 
 	#Change satellite positions to observer frame
-	x = np.zeros((observer_locations,3))
-
-	for i in range(observer_locations/2):
-        x[i*2][i] = observer_distance
-        x[i*2+1][i] = -1.*observer_distance
-
-	pos = pos - x[np.random.randint(0,observer_locations)]
+	pos_observer_frame = transform_to_observer_frame(pos)
 
 	#Set LMC coordinates (still assumes LMC analog is 0th subhalo)
-	mock_lmc_coords = (pos[0][0], pos[0][1], pos[0][2])
+	mock_lmc_coords = (pos_observer_frame[0][0], pos_observer_frame[0][1], pos_observer_frame[0][2])
 	true_lmc_coords = lmc_cartesian_coords
 
 	#Perform coordinate rotation
-	a,b = (mock_lmc_coords/np.linalg.norm(mock_lmc_coords)), (true_lmc_coords/np.linalg.norm(true_lmc_coords))
-    
-	v = np.cross(a,b)
-	c = np.dot(a,b)
-	s = np.linalg.norm(v)
-	I = np.identity(3)
-	vXStr = '{} {} {}; {} {} {}; {} {} {}'.format(0, -v[2], v[1], v[2], 0, -v[0], -v[1], v[0], 0)
-	k = np.matrix(vXStr)
-	r = I + k + np.matmul(k,k) * ((1 -c)/(s**2))
-
-	for i in range (0,len(rotated_pos)):
-        mock_lmc_coords = (rotated_pos[i][0],rotated_pos[i][1],rotated_pos[i][2])
-        (rotated_pos[i][0],rotated_pos[i][1],rotated_pos[i][2]) = np.asarray(r*np.reshape(mock_lmc_coords,(3,1)))[:,0]
-
+	rotated_pos = rotate_about_vector(pos_observer_frame,mock_lmc_coords,true_lmc_coords)
 	satellite_properties['rotated_pos'] = rotated_pos
 
 	return satellite_properties
