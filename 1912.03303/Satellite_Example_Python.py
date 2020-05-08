e3e5a4fd6898f1e0b36e92b82145fa5d6958caeb
Ethan Nadler <enadler@stanford.edu>
2020-05-08 15:29:13 -0700

    push in preparation of likelihood integration
    some functionality in masks.py is duplicated from ugali (https://github.com/DarkEnergySurvey/ugali)

--- 1912.03303/Satellite_Example_Python.py
+++ 1912.03303/Satellite_Example_Python.py
@@ -29,3 +29,35 @@ lmc_ind = 0
 lmc_cartesian_coords = get_lmc_coords(halo_data[0],cosmo_params,lmc_ind)
 satellite_properties_rotated = rotate_about_LMC(satellite_properties,halo_data[0],cosmo_params,
                                                 lmc_cartesian_coords,lmc_ind)
+
+#Precompute satellite properties and flags for each observer location
+satellite_properties_list = []
+DES_flags_list = []
+PS1_flags_list = []
+Halo_ra_list = []
+Halo_dec_list = []
+
+print('loading masks ...')
+des_mask = load_mask('des')
+ps1_mask = load_mask('ps1')
+print('done')
+
+for i in range(6):
+    #Get satellite properties
+    satellite_properties = get_satellite_properties(halo_data[14], params, hparams, cosmo_params, vpeak_Mr_interp)
+    satellite_properties_rotated = rotate_about_LMC(satellite_properties,halo_data[14],cosmo_params,
+                                                lmc_cartesian_coords,lmc_ind,i)
+    #Transform to sky coordinates
+    Halo_sky_coord = SkyCoord(x=satellite_properties_rotated['rotated_pos'][:,0], 
+                      y=satellite_properties_rotated['rotated_pos'][:,1], 
+                      z=satellite_properties_rotated['rotated_pos'][:,2], 
+                      unit='kpc', representation_type='cartesian').spherical
+    satellite_properties_rotated['ra'] = Halo_sky_coord.lon.degree
+    satellite_properties_rotated['dec'] = Halo_sky_coord.lat.degree
+    #Assign flags 
+    satellite_properties_rotated['DES_flags'] = evaluate_mask(Halo_sky_coord.lon.degree,Halo_sky_coord.lat.degree,
+                                                               des_mask)
+    satellite_properties_rotated['PS1_flags'] = evaluate_mask(Halo_sky_coord.lon.degree,Halo_sky_coord.lat.degree,
+                                                               ps1_mask)
+    #Append
+    satellite_properties_list.append(satellite_properties_rotated)
